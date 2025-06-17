8da184e33d5e9639f52cbbbde54c8b77bff4d3b3
okadengt <y-goto@oka-dengyo.co.jp>
2025-06-17 13:11:14 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
-[Unit]
-Description=PC Asset Server
-After=network.target
+from flask import Flask
 
-[Service]
-User=dengyo
-WorkingDirectory=/home/dengyo/pc-asset-server
-Environment="PATH=/home/dengyo/pc-asset-server/venv/bin"
-ExecStart=/home/dengyo/pc-asset-server/venv/bin/python run.py --host 0.0.0.0 --port 5000
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    from .views import bp
+    app.register_blueprint(bp)
+    return app
